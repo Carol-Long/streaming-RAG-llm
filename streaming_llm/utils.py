b7a6d084a5e3e5977255e45d7c1356ce9f4d0cd3
Carol Xuan Long <47681474+Carol-Long@users.noreply.github.com>
2023-11-29 11:25:37 -0500

    Fix the issue of RAM running out
    fix specified in here: https://huggingface.co/blog/accelerate-large-models

--- streaming_llm/utils.py
+++ streaming_llm/utils.py
@@ -60,6 +60,7 @@ def load(model_name_or_path):
         device_map="auto",
         torch_dtype=torch.float16,
         offload_folder="offload",
+        offload_state_dict = True,
         trust_remote_code=True,
     )
     if tokenizer.pad_token_id is None:
