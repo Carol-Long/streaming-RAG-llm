f6f95ee8a4cb0417f103fd4d403006136425c371
Carol Xuan Long <carol_long@g.harvard.edu>
2023-11-30 17:26:07 -0500

    Fixed Question json and Argument on when to generate response

--- data/generate_RAG_test.py
+++ data/generate_RAG_test.py
@@ -2,43 +2,33 @@ import random
 import json
 
 # Function to generate a line in the specified format
-def generate_line(line_number, count):
-    random_numbers = [str(random.randint(1, 1000)) for _ in range(count)]
-    line = f"Line {line_number}: {' '.join(random_numbers)}"
+def generate_line(line_number):
+    random_number = str(random.randint(1, 1000000))
+    line = f"Line {line_number}: {random_number}"
     return line
 
-# Generate 200 lines
-lines = [generate_line(line_number, 23) for line_number in range(1, 211)]
 
 result_strings = []
 reference_strings = []
-# Combine the lines into a single string
+
 for i in range(11):
-    if i != 10:
-        result_string = '\n'.join(lines[i*20:(i+1)*20])
-    else:
-        result_string = '\n'.join(lines[i*20:])
-    random_line = random.randint(1, 20)
-    add_question = f"What is the first number in line {str(random_line+1)}?"
-    result_string = result_string + '\n' + add_question
-    result_strings.append(result_string)
+    # Generate 200 lines in each question
+    lines = [generate_line(line_number) for line_number in range(200)]
+    random_line = random.randint(1, 200)
+    add_question = f"What is the number in Line {str(random_line)}?"
+    lines.append(add_question)
+
+    result_strings.append(lines)
     reference_strings.append(lines[random_line].split(' ')[2])
 
-# Define a list of dictionaries with the desired format
-# questions = [
-#     {"question_id": 81, "category": "extraction", "turns": result_strings, "reference": [lines[2].split(' ')[2]]},
-# ]
 questions = []
 for i in range(11):
-    questions.append({"question_id": i, "category": "extraction", "turns": result_strings[i], "reference": [reference_strings[i]]})
+    questions.append({"question_id": i+81, "category": "extraction", "turns": result_strings[i], "reference": [reference_strings[i]]})
 # Convert the list of dictionaries to a JSON-formatted string
 json_data = json.dumps(questions, indent=2)
+
 
 # Write the JSON-formatted string to a file
 with open("questions.jsonl", "w") as jsonl_file:
     for question in questions:
         jsonl_file.write(json.dumps(question) + '\n')
-
-# print(lines[0:2])
-# print(lines[30].split(' ')[2])
-# print(lines[31].split(' ')[2])
--- examples/run_streaming_llama.py
+++ examples/run_streaming_llama.py
@@ -70,9 +70,10 @@ def streaming_inference(model, tokenizer, prompts, kv_cache=None, max_gen_len=10
             space_needed = seq_len + max_gen_len
             past_key_values = kv_cache.evict_for_space(past_key_values, space_needed)
 
-        past_key_values = greedy_generate(
-            model, tokenizer, input_ids, past_key_values, max_gen_len=max_gen_len
-        )
+        if prompt[0] != "L":
+            past_key_values = greedy_generate(
+                model, tokenizer, input_ids, past_key_values, max_gen_len=max_gen_len
+            )
 
 
 def main(args):
