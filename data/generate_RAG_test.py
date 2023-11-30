ca5af2efd110b5073316a428f270ec4a97898a7c
Carol Xuan Long <carol_long@g.harvard.edu>
2023-11-30 16:34:33 -0500

    Change into multiple questions

--- data/generate_RAG_test.py
+++ data/generate_RAG_test.py
@@ -11,20 +11,26 @@ def generate_line(line_number, count):
 lines = [generate_line(line_number, 23) for line_number in range(1, 211)]
 
 result_strings = []
+reference_strings = []
 # Combine the lines into a single string
 for i in range(11):
     if i != 10:
         result_string = '\n'.join(lines[i*20:(i+1)*20])
     else:
         result_string = '\n'.join(lines[i*20:])
+    random_line = random.randint(1, 20)
+    add_question = f"What is the first number in line {str(random_line+1)}?"
+    result_string = result_string + '\n' + add_question
     result_strings.append(result_string)
+    reference_strings.append(lines[random_line].split(' ')[2])
 
-result_strings.append("What is the first number in line 3?")
 # Define a list of dictionaries with the desired format
-questions = [
-    {"question_id": 81, "category": "extraction", "turns": result_strings, "reference": [lines[2].split(' ')[2]]},
-]
-
+# questions = [
+#     {"question_id": 81, "category": "extraction", "turns": result_strings, "reference": [lines[2].split(' ')[2]]},
+# ]
+questions = []
+for i in range(11):
+    questions.append({"question_id": i, "category": "extraction", "turns": result_strings[i], "reference": [reference_strings[i]]})
 # Convert the list of dictionaries to a JSON-formatted string
 json_data = json.dumps(questions, indent=2)
 
