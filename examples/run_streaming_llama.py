0aa13cc949e05566df2201dcf9181b574a3d08a1
Carol Xuan Long <carol_long@g.harvard.edu>
2023-12-05 16:18:02 -0500

    fix bug

--- examples/run_streaming_llama.py
+++ examples/run_streaming_llama.py
@@ -95,7 +95,7 @@ def streaming_inference(model, tokenizer, prompts, kv_cache=None, max_gen_len=10
         if kv_cache is not None:
             space_needed = seq_len + max_gen_len
             past_key_values = kv_cache.evict_for_space(past_key_values, space_needed)
-        if prompt[0:21] == "Can you please repeat":       
+        if prompt[-9:] == "Story 10.":       
             past_key_values = greedy_generate(
                     model, tokenizer, input_ids, past_key_values, max_gen_len=max_gen_len
                 )
