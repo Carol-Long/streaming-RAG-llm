b1016541a54ad4c54631abf8a101b8afbc4a88d9
Carol Xuan Long <carol_long@g.harvard.edu>
2023-12-09 16:19:51 -0500

    Update run_streaming_llama.py

--- examples/run_streaming_llama.py
+++ examples/run_streaming_llama.py
@@ -84,17 +84,17 @@ def streaming_inference(model, tokenizer, prompts, kv_cache=None, max_gen_len=10
             evicted_data = []
             # evicted_indices = []
 
-        # if past_key_values:
-        #     print(len(past_key_values))
-        #     print(len(past_key_values[0]))
-        #     print(past_key_values[0][0].size())
-        #     print(len(evicted_data))
-        #     print(len(evicted_data[0]))
-        #     print(evicted_data[0][0].size())
-        #     past_key_values= past_key_values[:4] + evicted_data + past_key_values[4:]
-        #     print(len(past_key_values))
-        #     print(len(past_key_values[0]))
-        #     print(past_key_values[0][0].size())
+        if past_key_values:
+            print(len(past_key_values))
+            print(len(past_key_values[0]))
+            print(past_key_values[0][0].size())
+            print(len(evicted_data))
+            print(len(evicted_data[0]))
+            print(evicted_data[0][0].size())
+            past_key_values= past_key_values[:4] + evicted_data + past_key_values[4:]
+            print(len(past_key_values))
+            print(len(past_key_values[0]))
+            print(past_key_values[0][0].size())
             # for idx, kv_pair in zip(evicted_indices, evicted_data):
             #     if idx < len(past_key_values):
             #         past_key_values[idx] = kv_pair
